ec76fcfcb071fb0cadc4c131baa74019fc9ac870
Saatvik <srl55@cam.ac.uk>
2022-11-19 22:33:43 +0000

    Complete csv parser

--- scenarios/interface.py
+++ scenarios/interface.py
@@ -1,3 +1,5 @@
+import random
+
 LOREM_IPSUM = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas convallis hendrerit felis, ut porttitor felis facilisis ac. Aliquam aliquet ultricies odio, sit amet varius nibh. Fusce congue est ut tempus feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam tincidunt lacus quis quam sollicitudin congue. Donec tristique dignissim fringilla. Quisque posuere, orci nec tristique lobortis, nunc leo imperdiet tellus, quis porttitor tortor lectus sit amet nisi. Aliquam tristique auctor ipsum ultricies ultrices.
 
 Donec pulvinar volutpat neque sed varius. Etiam vulputate ante ac sapien laoreet, ut feugiat mauris varius. Nulla ut placerat libero, ac mollis nisl. Pellentesque placerat ex libero, nec consectetur velit ultricies eget. Vivamus massa sapien, finibus et vulputate vel, venenatis vitae augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. In posuere sapien vitae ante pellentesque eleifend. Quisque mattis massa non maximus suscipit. In in lectus nec libero malesuada malesuada. Donec vitae dui vel elit venenatis vulputate. Duis tincidunt ut leo fermentum fringilla. Proin lacinia metus id tincidunt vestibulum.'''
@@ -13,9 +15,10 @@ def gen_scenario():
 
 def randomise_paramters():
     return {
-        "cares_about": 0.,
-        "no_nukes": 0.,
-        "agression": 0.,
-        "international_rep": 0.,
-        "population": 0.
+        "cares_about": random.random(),
+        "no_nukes": random.random(),
+        "agression": random.random(),
+        "international_rep": random.random(),
+        "population": random.random()
     }
+
