65054998ecfb5361a598b5fded4746ec181bd2ef
Oliver Gaskell <og305@cam.ac.uk>
2022-11-20 14:19:37 +0000

    Add labels to showcase form

--- website/mysite/forms.py
+++ website/mysite/forms.py
@@ -12,18 +12,18 @@ pprint(paramdescs)
 formoptions = [[((desc["max"] + desc["min"]) / 2, desc["description"]) for desc in paramdescs[name]] for name in paramnames]
 
 class ShowcaseForm(forms.Form):
-    a_ca = forms.CharField(label="", widget=forms.Select(choices=formoptions[0]))
-    a_nn = forms.CharField(label="", widget=forms.Select(choices=formoptions[1]))
-    a_a  = forms.CharField(label="", widget=forms.Select(choices=formoptions[2]))
-    a_ir = forms.CharField(label="", widget=forms.Select(choices=formoptions[3]))
-    a_p  = forms.CharField(label="", widget=forms.Select(choices=formoptions[4]))
-    a_pr = forms.CharField(label="", widget=forms.Select(choices=formoptions[5]))
+    a_ca = forms.CharField(label="Country A: Cares About", widget=forms.Select(choices=formoptions[0]))
+    a_nn = forms.CharField(label="Country A: Number of Nukes", widget=forms.Select(choices=formoptions[1]))
+    a_a  = forms.CharField(label="Country A: Agression", widget=forms.Select(choices=formoptions[2]))
+    a_ir = forms.CharField(label="Country A: International Reputation", widget=forms.Select(choices=formoptions[3]))
+    a_p  = forms.CharField(label="Country A: Population", widget=forms.Select(choices=formoptions[4]))
+    a_pr = forms.CharField(label="Provocation", widget=forms.Select(choices=formoptions[5]))
 
-    b_ca = forms.CharField(label="", widget=forms.Select(choices=formoptions[0]))
-    b_nn = forms.CharField(label="", widget=forms.Select(choices=formoptions[1]))
-    b_a  = forms.CharField(label="", widget=forms.Select(choices=formoptions[2]))
-    b_ir = forms.CharField(label="", widget=forms.Select(choices=formoptions[3]))
-    b_p  = forms.CharField(label="", widget=forms.Select(choices=formoptions[4]))
+    b_ca = forms.CharField(label="Country B: Cares About", widget=forms.Select(choices=formoptions[0]))
+    b_nn = forms.CharField(label="Country B: Number of Nukes", widget=forms.Select(choices=formoptions[1]))
+    b_a  = forms.CharField(label="Country B: Agression", widget=forms.Select(choices=formoptions[2]))
+    b_ir = forms.CharField(label="Country B: International Reputation", widget=forms.Select(choices=formoptions[3]))
+    b_p  = forms.CharField(label="Country B: Population", widget=forms.Select(choices=formoptions[4]))
 
 if __name__ == "__main__":
     from pprint import pprint
